b67bcf85737851d749da2026752ed64aa02ff506
Michel Geronazzo <40324940+michelgeronazzo@users.noreply.github.com>
2025-10-04 17:14:42 -0300

    Add files via upload

---
+++ Shared/Dto/AlterarStatusDto.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Dto
+{
+    public record AlterarStatusDto(PropostaStatus Status);
+}
